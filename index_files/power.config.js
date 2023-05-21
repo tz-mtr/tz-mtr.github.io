72d8ab77cb160aa5e5b62f9c762d146e11d3e9fe
天州地铁 <134213043+tz-mtr@users.noreply.github.com>
2023-05-21 22:07:28 +0800

    Add files via upload

---
+++ index_files/power.config.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    'use strict';
+
+    window.POWER_GRAYSCALE_CONFIG = {"Enabled":false,"StartDateTime":"2020-03-01T06:25:03","EndDateTime":"2020-04-14T15:45:02"};
+    window.POWER_LEAVESITEPROMPTS_CONFIG = {"LeaveSitePrompts":{"0":"OnShow","1":"PlatformConfig"}};
+}());
